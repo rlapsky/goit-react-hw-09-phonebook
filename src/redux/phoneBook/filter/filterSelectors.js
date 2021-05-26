9fe39d17b7551afbd91f71e95787b8f959bfc580
rlapsky <61615744+rlapsky@users.noreply.github.com>
2021-05-26 14:20:30 +0300

    end

---
+++ src/redux/phoneBook/filter/filterSelectors.js
@@ -0,0 +1,11 @@
+import { createSelector} from '@reduxjs/toolkit'
+import { getAllItems } from '../items/itemsSelectirs';
+export const getFilter = (state) => state.contacts.filter;
+
+
+export const getFilteredContacts = createSelector([getAllItems, getFilter], (items,filter) => {
+    const formatedFilter = filter?.toLowerCase(); 
+    return items?.filter((contact) =>
+      contact.name.toLowerCase().includes(formatedFilter)
+    );
+});
